e8aaca081d0d025fc0151d2c406fa50c43e456dc
Dee Race <deerace12@gmail.com>
2018-06-18 15:40:43 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presense: true
   validates :phone_number, length: {is: 10}
-  validates :email, presense: true, uniquesness: true
+  validates :email, uniquesness: true
 end
